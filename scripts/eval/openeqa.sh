b199b8ca71cadb08e379a695f22ef264280a4f8d
NoahLodes <go98tiq@mytum.de>
2025-01-06 14:27:56 +0100

    clean up

--- scripts/eval/openeqa.sh
+++
@@ -1,4 +0,0 @@
- python llava/eval/model_openeqa.py \
-        --model-path ./checkpoints/llava-3d-7b \
-        --question-file playground/data/annotations/llava-3d-openeqa.json \
-        --answers-file ./llava-3d-7b-openeqa_pred.json
--- scripts/eval/scan2cap.sh
+++
@@ -1,4 +0,0 @@
-python llava/eval/model_scan2cap.py \
-        --model-path ./checkpoints/llava-3d-7b \
-        --question-file playground/data/annotations/llava-3d-scan2cap_val.json \
-        --answers-file ./llava-3d-7b-scan2cap_val.json
