58eccbf164ee2c8f29502c2104ba1ed24f035bd4
Noah Lodes <noah.lodes@gmx.de>
2025-01-25 13:16:29 +0100

    add constants

--- constants.py
+++ constants.py
@@ -1,2 +1,9 @@
-CONF_PATH_R3SCAN_RAW = 'data/3rscan'
-CONF_PATH_R3SCAN_PROCESSED = 'data/3rscan/processed'
+def get_paths(name):
+    if name=="Noah":
+        blip_cache_dir = '/content/drive/MyDrive/master_practical/models/BLIP'
+        llava_cache_dir = '/content/drive/MyDrive/master_practical/models/LLaVA3D'
+        path_3rscan_raw = '/content/drive/MyDrive/master_practical/data/3RScan'
+        path_3rscan_metadata = './data/3RScan'
+        path_3rscan_processed = './data/3RScan/processed'
+
+    return blip_cache_dir, llava_cache_dir, path_3rscan_raw, path_3rscan_metadata, path_3rscan_processed
