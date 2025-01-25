803a7ac6b67d878db0ef76fc234ded6365b7ba31
NoahLodes <go98tiq@mytum.de>
2025-01-25 22:52:39 +0100

    change constants

--- constants.py
+++ constants.py
@@ -2,8 +2,8 @@ def get_paths(name):
     if name=="Noah":
         blip_cache_dir = '/content/drive/MyDrive/master_practical/models/BLIP'
         llava_cache_dir = '/content/drive/MyDrive/master_practical/models/LLaVA3D'
-        path_3rscan_raw = '/content/drive/MyDrive/master_practical/data/3RScan'
-        path_3rscan_metadata = './data/3RScan'
-        path_3rscan_processed = './data/3RScan/processed'
+        path_3rscan_raw = '/content/drive/MyDrive/master_practical/data/3rscan'
+        path_3rscan_metadata = './data/3rscan'
+        path_3rscan_processed = './data/3rscan/processed'
 
     return blip_cache_dir, llava_cache_dir, path_3rscan_raw, path_3rscan_metadata, path_3rscan_processed
