7b2f2f7fa9c7178d117b35ce5de16bf22089a6b1
NoahLodes <go98tiq@mytum.de>
2025-01-25 15:05:25 +0100

    add cache position arg

--- llava/model/language_model/llava_llama.py
+++ llava/model/language_model/llava_llama.py
@@ -73,6 +73,7 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
         clicks: Optional[List[List[float]]] = None,
         image_sizes: Optional[List[List[int]]] = None,
         return_dict: Optional[bool] = None,
+        cache_position=None
     ) -> Union[Tuple, CausalLMOutputWithPast]:
 
         if inputs_embeds is None:
