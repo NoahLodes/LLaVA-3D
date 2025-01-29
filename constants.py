9ade1a86bec34c1e42c28fccc80734951bd055f9
elenaalegret <elenaalegret@gmail.com>
2025-01-29 17:12:17 +0100

    Update constant conflicts

--- constants.py
+++ constants.py
@@ -5,5 +5,13 @@ def get_paths(name):
         path_3rscan_raw = '/content/drive/MyDrive/master_practical/data/3rscan'
         path_3rscan_metadata = './data/3rscan'
         path_3rscan_processed = './data/3rscan/processed'
+    
+    elif name=="Elena":
+        blip_cache_dir = '/content/drive/MyDrive/4tCarrera/master_practical/models/BLIP'
+        llava_cache_dir = '/content/drive/MyDrive/4tCarrera/master_practical/models/LLaVA3D'
+        path_3rscan_raw = '/content/drive/MyDrive/4tCarrera/master_practical/data/3rscan'
+        path_3rscan_metadata = './data/3rscan'
+        path_3rscan_processed = './data/3rscan/processed'
+
 
     return blip_cache_dir, llava_cache_dir, path_3rscan_raw, path_3rscan_metadata, path_3rscan_processed
